efd5bb922c9ceca8b6b5a2d590b27cd3906c79ed
Cody Lemons <codylemons@Codys-MacBook-Pro.local>
2018-08-24 12:53:06 -0600

    bloccit restarted

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,13 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+
+  def about
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'random_data'
+
+ # Create Posts
+ 50.times do
+ # #1
+   Post.create!(
+ # #2
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
+   )
+ end
+
+Post.find_or_create_by!(
+         title: "the week rushed by",
+         body: "it's weird how I lose track of time sometimes"
+)
+
+ posts = Post.all
+ 
+ # Create Comments
+ # #3
+ 100.times do
+   Comment.create!(
+ # #4
+     post: posts.sample,
+     body: RandomData.random_paragraph
+   )
+ end
+
+ Comment.find_or_create_by!(
+       post: Post.find(101),
+       body: "music makes the world go round"
+)
+ 
+ puts "Seed finished"
+ puts "#{Post.count} posts created"
+ puts "#{Comment.count} comments created"
