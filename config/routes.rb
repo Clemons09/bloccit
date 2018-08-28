38e4e62a58ddfc433d50a44326257723daa27d9c
Cody Lemons <codylemons@Codys-MacBook-Pro.local>
2018-08-27 20:34:10 -0600

    checkpoint 23 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   
-  get 'sponsored_posts/show'
-  get 'sponsored_posts/new'
-  get 'sponsored_posts/edit'
   resources :topics do
     resources :posts, except: [:index]
   end
+
+  resources :users, only: [:new, :create]
 
   get 'about' => 'welcome#about'
 
