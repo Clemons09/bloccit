32d4b5dae352a39dc6b3949bdd4fd7a2a902b35e
Cody Lemons <codylemons@Codys-MacBook-Pro.local>
2018-09-06 11:39:51 -0600

    checkpoint 26 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,30 @@
 require 'random_data'
 
- # Create Posts
+5.times do
+  User.create!(name: RandomData.random_name, email: RandomData.random_email, password: RandomData.random_sentence)
+end
+users = User.all
+
+15.times do
+  Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+end
+topics = Topic.all
+
 50.times do
-    Post.create!(
-         topic: topics.sample,
-         title: RandomData.random_sentence,
-         body:  RandomData.random_paragraph
-    )
+  Post.create!(user: users.sample, topic: topics.sample, title: RandomData.random_sentence, body: RandomData.random_paragraph)
 end
 
 posts = Post.all
- 
- # Create Comments
- # #3
- 100.times do
-   Comment.create!(
- # #4
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
 
-115.times do
-    Topic.create!(
-        name:         RandomData.random_sentence,
-        description:  RandomData.random_paragraph
-    )
+100.times do
+  Comment.create!(post: posts.sample, body: RandomData.random_paragraph)
 end
-topics = Topic.all
- 
- puts "Seed finished"
- puts "#{Topic.count} topics created"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+
+user = User.first
+user.update_attributes!(email: "titch@hotmail.co.uk", password: "motomoto")
+
+puts "Seed Finished"
+puts "#{User.count} users created"
+puts "#{Topic.count} topics created"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
